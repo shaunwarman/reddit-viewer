5ab2ee3463f659abba6362d05a78b76b1b2c8597
Shaun Warman <shaunwarman1@gmail.com>
2014-06-07 01:39:47 -0500

    Search by topic

--- controllers/index.js
+++ controllers/index.js
@@ -9,9 +9,9 @@ module.exports = function (app) {
     var model;
 
 
-    app.get('/', function (req, res) {
-        
-        var search = request("http://www.reddit.com/r/programming/top.json", function (error, response, body) {
+    app.get('/:topic', function (req, res) {
+        var topic = req.params.topic;
+        var search = request("http://www.reddit.com/r/"+topic+"/top.json", function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 		  		model = JSON.parse(body);
 			}
