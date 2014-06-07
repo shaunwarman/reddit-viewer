dab07aa8a66a62136800842c581f16f5238c5e48
Shaun Warman <shaunwarman1@gmail.com>
2014-06-07 13:47:06 -0500

    More clean-up

--- controllers/index.js
+++ controllers/index.js
@@ -5,19 +5,16 @@ var IndexModel = require('../models/index');
 var request = require('request');
 
 module.exports = function (app) {
-
-    var model,
-    	search;
-
+    var model;
 
     app.get('/:topic', function (req, res) {
         var topic = req.params.topic;
         
-        search = request("http://www.reddit.com/r/"+topic+"/top.json", function (error, response, body) {
-			if (!error && response.statusCode == 200) {
+        request("http://www.reddit.com/r/"+topic+".json", function (error, response, body) {
+			if (!error && response.statusCode === 200) {
 		  		model = JSON.parse(body);
 			}
-			
+            console.log(model.data.children[1]);
     		res.render('reddit', model);
 		});
     });
