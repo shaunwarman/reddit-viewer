d3ad4b7964d3cfb4d00643eda3f923d3d361645b
Shaun Warman <shaunwarman1@gmail.com>
2014-06-09 22:21:03 -0500

    Easier way to push subreddit

--- controllers/index.js
+++ controllers/index.js
@@ -14,9 +14,8 @@ module.exports = function (app) {
 			if (!error && response.statusCode === 200) {
 		  		model = JSON.parse(body);
 			}
-            var subtitle = model.data.children[0].data.subreddit;
-            model.subtitle = subtitle;
-            
+            model.subtitle = topic;
+
     		res.render('reddit', model);
 		});
     });
