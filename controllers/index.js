20541fce66777d1ff04cb42a7e4f2c40bcef4759
Shaun Warman <swarman@paypal.com>
2014-11-22 23:05:45 -0600

    Lookup top articles

--- controllers/index.js
+++ controllers/index.js
@@ -6,15 +6,17 @@ var request = require('request');
 
 module.exports = function (app) {
     var model;
+    var top = "/top"
 
     app.get('/:topic', function (req, res) {
         var topic = req.params.topic;
-        
-        request("http://www.reddit.com/r/"+topic+".json", function (error, response, body) {
+
+        request("http://www.reddit.com/r/"+topic+top+".json", function (error, response, body) {
 			if (!error && response.statusCode === 200) {
 		  		model = JSON.parse(body);
 			}
             model.subtitle = topic;
+            console.log("Model: " + JSON.stringify(model));
 
     		res.render('reddit', model);
 		});
