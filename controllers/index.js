121fc549bf8bba788442f1d5fce92ecce9ac853f
Shaun Warman <swarman@paypal.com>
2015-02-14 23:31:50 -0600

    Update query and add filter

--- controllers/index.js
+++ controllers/index.js
@@ -3,22 +3,28 @@
 
 var IndexModel = require('../models/index');
 var request = require('request');
+var cleaner = require('../lib/cleaner');
 
 module.exports = function (app) {
     var model;
-    var top = "/top"
+    var top = "/top";
 
     app.get('/:topic', function (req, res) {
         var topic = req.params.topic;
+        var query = "?sort=top&t=week&limit=100"
 
-        request("http://www.reddit.com/r/"+topic+top+".json", function (error, response, body) {
+        request("http://www.reddit.com/r/"+topic+top+".json"+query, function (error, response, body) {
 			if (!error && response.statusCode === 200) {
 		  		model = JSON.parse(body);
+                console.log(model);
 			}
             model.subtitle = topic;
-            console.log("Model: " + JSON.stringify(model));
 
-    		res.render('reddit', model);
+            var articles = cleaner.filterLinks(model, function(links) {
+                model.links = links;
+                res.render('reddit', model);
+            });
 		});
+
     });
 };
